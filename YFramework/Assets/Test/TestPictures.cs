8fc1b43fdcfda2014e43f9919a7101e1ad522323
Y <97585120+hubuliuming@users.noreply.github.com>
2022-06-17 17:34:35 +0800

    待修改Ypicture

--- YFramework/Assets/Test/TestPictures.cs
+++ YFramework/Assets/Test/TestPictures.cs
@@ -6,17 +6,32 @@
     功能：Nothing
 *****************************************************/
 
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using YFramework;
 
-public class TestPictures : MonoBehaviour
+public class TestPictures : YMonoBehaviour
 {
-    public RectTransform area;
-    public Rect rect;
+   
+    public  RectTransform areaTran;
+    private string path = "D:/拍照照片";
     private void Start()
     {
-     
+        Delay(0.01f, ()=>
+        {
+            // YPicture pic = new YPicture(areaTran);
+            // var data = pic.GetData();
+            // Debug.Log(data);
+            // pic.SaveLocalFile(path,data);
+            
+            Texture2D texture2D = new Texture2D((int) areaTran.sizeDelta.x, (int) areaTran.sizeDelta.y, TextureFormat.ARGB32, false);
+            texture2D.ReadPixels(areaTran.rect,0,0,false);
+            var data = texture2D.EncodeToPNG();
+            texture2D.Apply();
+            File.WriteAllBytes(path+"222.PNG",data);
+        });
+      
     }
-
-    
+   
 }
--- YFramework/Assets/YFramework/Framework/Common/YPicture.cs
+++ YFramework/Assets/YFramework/Framework/Common/YPicture.cs
@@ -6,13 +6,14 @@
     功能：挂载到UI物体上生成对应的图片
 *****************************************************/
 
+using System;
 using System.Collections;
 using System.IO;
 using UnityEngine;
 
 namespace YFramework
 {
-    public class YPicture : MonoBehaviour 
+    public class YPicture 
     {
         public enum PictureType
         {
@@ -22,7 +23,7 @@ namespace YFramework
             TGA
         }
 
-        public PictureType Type;
+        private PictureType _type;
         private RectTransform _rectTrans;
         private byte[] _data;
         private string _defaultName;
@@ -31,20 +32,47 @@ namespace YFramework
         private int _width;
         private int _height;
 
-        private void Start()
+        public YPicture(RectTransform rectTrans,PictureType type = PictureType.PNG)
         {
-            _rectTrans = GetComponent<RectTransform>();
-            _startX = (int)_rectTrans.offsetMin.x;
-            _startY = (int)_rectTrans.offsetMin.y;
-            _width = (int) (Screen.width - _startX + _rectTrans.offsetMax.x);
-            _height = (int) (Screen.height - _startY + _rectTrans.offsetMax.y);
+            this._rectTrans = rectTrans;
+            this._type = type;
+            _defaultName = DateTime.Now.ToString("yyyyMMddHHmmss");
+            _startX = (int)_rectTrans.position.x;
+            _startY = (int)_rectTrans.position.y;
+            _width = (int)_rectTrans.sizeDelta.x;
+            _height = (int)_rectTrans.sizeDelta.y;
+            Debug.Log(_startX);
+            Debug.Log(_startY);
+            Debug.Log(_width);
+            Debug.Log(_height);
         }
 
-        public byte[] GetData() => GetData(Type);
+        public byte[] GetData() => GetData(_type);
 
         public byte[] GetData(PictureType type)
         {
-            StartCoroutine(CorCut(type));
+            Texture2D texture2D = new Texture2D(_width, _height, TextureFormat.ARGB32, false);
+            texture2D.ReadPixels(new Rect(_startX,_startY,_width,_height),0,0,false);
+            texture2D.Apply();
+        
+            switch (type)
+            {
+                case PictureType.PNG:
+                    _data = texture2D.EncodeToPNG();
+                    break;
+                case PictureType.JPG:
+                    _data = texture2D.EncodeToJPG();
+                    break;
+                case PictureType.EXR:
+                    _data = texture2D.EncodeToEXR();
+                    break;
+                case PictureType.TGA:
+                    _data = texture2D.EncodeToTGA();
+                    break;
+            }
+
+            if (_data == null)
+                Debug.LogError("转化图片失败");
             return _data;
         }
 
@@ -74,16 +102,9 @@ namespace YFramework
             if (_data == null)
                 Debug.LogError("转化图片失败");
         }
-        public byte[] CutByWebCam(WebCamTexture webCamTexture) => CutByWebCam(webCamTexture, Type);
+        public byte[] CutByWebCam(WebCamTexture webCamTexture) => CutByWebCam(webCamTexture, _type);
         public byte[] CutByWebCam(WebCamTexture webCamTexture,PictureType type)
         {
-            StartCoroutine(CorWebCam(webCamTexture, type));
-            return _data;
-        }
-
-        private IEnumerator CorWebCam(WebCamTexture webCamTexture,PictureType type)
-        {
-            yield return new WaitForEndOfFrame();
             Texture2D texture2D = new Texture2D(webCamTexture.width, webCamTexture.height, TextureFormat.ARGB32, false);
             Color[] colors = webCamTexture.GetPixels();
             texture2D.SetPixels(colors);
@@ -106,21 +127,21 @@ namespace YFramework
 
             if (data == null)
                 Debug.LogError("转化图片失败");
+            return _data;
         }
+        
 
         public void SaveLocalFile(string path,byte[] pictureData,PictureType type,string pictureName )
         {
-            //如果存储路径不存在，则创建文件夹
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
             File.WriteAllBytes(path + "/" + pictureName+"."+ type.ToString(), pictureData);
         }
-
-        public void SaveLocalFile(string path, byte[] pictureData, string pictureName) => SaveLocalFile(path, pictureData, Type, pictureName);
-        public void SaveLocalFile(string path, byte[] pictureData,PictureType type) => SaveLocalFile(path, pictureData, Type, _defaultName);
-        public void SaveLocalFile(string path, byte[] pictureData) => SaveLocalFile(path, pictureData, Type, _defaultName);
+        public void SaveLocalFile(string path, byte[] pictureData, string pictureName) => SaveLocalFile(path, pictureData, _type, pictureName);
+        public void SaveLocalFile(string path, byte[] pictureData,PictureType type) => SaveLocalFile(path, pictureData, _type, _defaultName);
+        public void SaveLocalFile(string path, byte[] pictureData) => SaveLocalFile(path, pictureData, _type, _defaultName);
 
     }
 }
