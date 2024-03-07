4a17cc99bb53083dce4401a42f1ab72c92a88e1e
Y <hubuliuming@gmail.com>
2024-03-07 14:32:54 +0800

    调整Kit中的UI文件到外一层级，新增CircleImage

---
+++ YFramework/Assets/YFramework/UI/Editor/CricleImageEditor.cs
@@ -0,0 +1,40 @@
+/****************************************************
+    文件：CricleImageEditor.cs
+    作者：Y
+    邮箱: 916111418@qq.com
+    日期：#CreateTime#
+    功能：Nothing
+*****************************************************/
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CircleImage),true)]
+[CanEditMultipleObjects]
+public class CricleImageEditor : UnityEditor.UI.ImageEditor
+{
+    private SerializedProperty _fillPercent;
+    private SerializedProperty _segement;
+    private SerializedProperty _h;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        _fillPercent = serializedObject.FindProperty("showPercent");
+        _segement = serializedObject.FindProperty("segments");
+      
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+        EditorGUILayout.Slider(_fillPercent, 0, 1, new GUIContent("showPercent"));
+        EditorGUILayout.PropertyField(_segement);
+        serializedObject.ApplyModifiedProperties();
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
