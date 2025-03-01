c28bfe4b6fd20ffe6853c8d7ea7e9e880dca6f1e
Y <hubuliuming@gmail.com>
2025-03-01 14:32:48 +0800

    fix

--- Assets/YFramework/Framework.cs
+++ Assets/YFramework/Framework.cs
@@ -916,12 +916,5 @@ namespace YFramework
     }
 
     #endregion
-
-#if UNITY_EDITOR
-    internal class EditorMenus
-    {
-        [UnityEditor.MenuItem("QFramework/Install QFrameworkWithToolKits")]
-        public static void InstallPackageKit() => UnityEngine.Application.OpenURL("https://qframework.cn/qf");
-    }
+    
 }
-#endif
