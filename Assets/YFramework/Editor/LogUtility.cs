73fca12b2629415f8b09d4ec4dec3f0f0a236b0a
Y <hubuliuming@gmail.com>
2025-07-21 15:12:59 +0800

    add

---
+++ Assets/YFramework/Editor/LogUtility.cs
@@ -0,0 +1,12 @@
+﻿namespace YFramework.Editor
+{
+    public class LogUtility
+    {
+        public static void ClearLog()
+        {
+            var logEntries = System.Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
+            var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            clearMethod.Invoke(null, null);
+        }
+    }
+}
