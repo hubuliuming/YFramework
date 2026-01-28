66be4a938c4ae8fe5b31c648bc35e8bb13d16931
Y <hubuliuming@gmail.com>
2026-01-28 19:06:16 +0800

    add simple timer

---
+++ Assets/YFramework/Kit/Time/TimerKit.cs
@@ -0,0 +1,27 @@
+/****************************************************
+    文件：TimerKit.cs
+    作者：Y
+    邮箱: 916111418@qq.com
+    日期：#CreateTime#
+    功能：Nothing
+*****************************************************/
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YFramework.Kit
+{
+    public class TimerKit 
+    {
+        #region 成员变量
+
+   
+        #endregion
+
+
+        // public static int Register(float updateInterval = 0)
+        // {
+        //     return TimerManager.Instance.Register(updateInterval);
+        // }
+    }
+}
