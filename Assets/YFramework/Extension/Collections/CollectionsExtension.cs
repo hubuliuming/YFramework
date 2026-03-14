392d8af53f451a0700431a46d0e87db55d8ac49d
Y <hubuliuming@gmail.com>
2026-03-14 20:29:09 +0800

    upd

--- Assets/YFramework/Extension/Collections/CollectionsExtension.cs
+++ Assets/YFramework/Extension/Collections/CollectionsExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace YFramework.Extension
@@ -13,6 +14,13 @@ namespace YFramework.Extension
         public static void StartCoroutine(this IEnumerator enumerator,MonoBehaviour mono)
         {
             mono.StartCoroutine(enumerator);
+        }
+        
+        public static void RemoveAtFast<T>(this List<T> list, int index)
+        {
+            int lastIndex = list.Count - 1;
+            list[index] = list[lastIndex];
+            list.RemoveAt(lastIndex);
         }
     }
 }
