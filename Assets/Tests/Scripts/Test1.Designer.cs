58f22a883a1489c27f7b6a323aa735952183ce03
Y <hubuliuming@gmail.com>
2026-04-17 09:47:42 +0800

    upd

--- Assets/Tests/Scripts/Test1.Designer.cs
+++ Assets/Tests/Scripts/Test1.Designer.cs
@@ -8,11 +8,11 @@ public partial class Test1
 	[YFramework.AutoBindField]
 	public UnityEngine.UI.Button BtnHide;
 	[YFramework.AutoBindField]
+	public UnityEngine.RectTransform Rectaaarr;
+	[YFramework.AutoBindField]
 	public CC.Test2 Test2;
 	[YFramework.AutoBindField]
 	public UnityEngine.GameObject GoHello;
 	[YFramework.AutoBindField]
 	public UnityEngine.UI.Image ImgHello;
-	[YFramework.AutoBindField]
-	public TMPro.TextMeshProUGUI TMPOO;
 }
