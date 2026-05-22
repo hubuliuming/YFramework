31d68fcd46d43a909d089e5752725f7954ae52e4
Y <hubuliuming@gmail.com>
2026-05-22 10:36:09 +0800

    upd

--- Assets/YFramework/Editor/Utility/LogUtility.cs
+++ Assets/YFramework/Editor/Utility/LogUtility.cs
@@ -6,7 +6,7 @@
         {
             var logEntries = System.Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
             var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
-            clearMethod.Invoke(null, null);
+            clearMethod?.Invoke(null, null);
         }
     }
 }
